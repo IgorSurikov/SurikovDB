1115435b50cef6bc4fafad5221cedd14610ccb2d
Igor Surikov <i.surikov@outlook.com>
2022-03-04 03:42:03 +0300

    DataBlock. Work with pointers

--- src/Block.py
+++ src/Block.py
@@ -1,8 +1,8 @@
 from struct import unpack_from, pack, calcsize
-from typing import Union, NoReturn
+from typing import NoReturn
 
 from src.TableMetaData import TableMetaData
-from src.constants import BLOCK_SIZE, BYTEORDER, POINTER_SIZE, POINTERS_PER_BLOCK, TABLE_IS_DELETED_F, TABLE_DDL_SIZE_F
+from src.constants import *
 
 
 class Block(bytearray):
@@ -35,8 +35,17 @@ class TableMetaDataBlock(Block):
     def __init__(self, data: bytes, index: int = 0):
         super().__init__(data, index)
 
+        self._ddl_size = unpack_from(f'{TABLE_DDL_SIZE_F}', self, 0)[0]
+        self._ddl_start_ptr = 0 + calcsize(f'{TABLE_DDL_SIZE_F}')
+
+        self._pointer_count_start_ptr = self._ddl_start_ptr + self._ddl_size
+        self._pointer_count = unpack_from(f'{POINTER_COUNT_F}', self, self._pointer_count_start_ptr)[0]
+
+        self._pointer_list_start_ptr = self._pointer_count_start_ptr + calcsize(f'{POINTER_COUNT_F}')
+        self._pointer_list_end_ptr = self._pointer_list_start_ptr + (self._pointer_count * calcsize(f'{POINTER_F}'))
+
     @classmethod
-    def from_block(cls, block: Block):
+    def from_block(cls, block: Block) -> 'TableMetaDataBlock':
         return cls(bytes(block), block.idx)
 
     def create_table(self, table_meta_data: TableMetaData) -> NoReturn:
@@ -48,16 +57,64 @@ class TableMetaDataBlock(Block):
 
         self.override(0, ddl)
 
-    def drop_table(self):
+    def drop_table(self) -> NoReturn:
         self.free()
 
     def get_table_meta_data(self) -> TableMetaData:
         if self.is_empty:
-            raise Exception('No TableMetadata')
+            raise Exception('No Table Metadata')
 
-        ptr = 0
-        ddl_size = unpack_from(f'{TABLE_DDL_SIZE_F}', self, ptr)[0]
-        ptr += calcsize(f'{TABLE_DDL_SIZE_F}')
-        return TableMetaData.from_encode_ddl(self[ptr: ptr + ddl_size])
+        return TableMetaData.from_encode_ddl(self[self._ddl_start_ptr: self._ddl_start_ptr + self._ddl_size])
+
+    def add_pointers(self, pointer_list: list[int]):
+
+        pointer_list_encode = pack(f'{POINTER_F}' * len(pointer_list), *pointer_list)
+
+        if self._pointer_list_end_ptr + len(pointer_list_encode) > BLOCK_SIZE:
+            raise Exception('No space for pointer list')
+
+        self.override(self._pointer_list_end_ptr, pointer_list_encode)
+        self._pointer_list_end_ptr += calcsize(f'{POINTER_F}' * len(pointer_list))
+        self._pointer_count += len(pointer_list)
+
+    def get_pointers(self) -> list[int]:
+        return list(
+            unpack_from(
+                f'{POINTER_F}' * self._pointer_count,
+                self,
+                self._pointer_list_start_ptr))
 
 
+class DataBlock(Block):
+
+    def __init__(self, data: bytes, index: int, row_format: str):
+        super().__init__(data, index)
+
+        self._row_format = row_format
+        self._row_count = unpack_from(f'{ROW_COUNT_F}', self, 0)[0]
+
+        self._row_list_start_ptr = 0 + calcsize(f'{ROW_COUNT_F}')
+        self._row_list_end_ptr = self._row_list_start_ptr + (self._row_count * calcsize(f'{row_format}'))
+
+    @classmethod
+    def from_block(cls, block: Block, row_format: str) -> 'DataBlock':
+        return cls(bytes(block), block.idx, row_format)
+
+    def add_rows(self, row_list: list[tuple]) -> NoReturn:
+
+        row_list_flat = sum(row_list, ())
+        row_list_encode = pack(f'{self._row_format}' * len(row_list), *row_list_flat)
+
+        if self._row_list_end_ptr + len(row_list_encode) > BLOCK_SIZE:
+            raise Exception('No space for row list')
+
+        self.override(self._row_list_end_ptr, row_list_encode)
+        self._row_list_end_ptr += calcsize(f'{self._row_format}' * len(row_list))
+        self._row_count += len(row_list)
+
+    def get_rows(self) -> list[tuple]:
+        return list(
+            unpack_from(
+                f'{self._row_format}' * self._row_count,
+                self,
+                self._row_list_start_ptr))
