937c414a70c6fdffd43eb69884ed776e298a0c19
Igor Surikov <i.surikov@outlook.com>
2022-04-13 02:34:29 +0300

    CLI

---
+++ src/SurikovDB/CLISurikovDB.py
@@ -0,0 +1,43 @@
+import argparse
+
+from SurikovDB.DBMS import DBMS
+
+parser = argparse.ArgumentParser(description="SurikovDB DBMS", prog='SurikovDB')
+subparsers = parser.add_subparsers()
+
+
+def list_db(args):
+    db_list = DBMS.get_data_base_list()
+    for i in db_list:
+        print(f"Name: {i['name']}, path: {i['path']}")
+
+
+def create_db(args):
+    pass
+
+
+def drop_db(args):
+    pass
+
+
+def start_db(args):
+    pass
+
+
+list_db_parser = subparsers.add_parser('list_db', help='List all databases')
+list_db_parser.set_defaults(func=list_db)
+
+create_db_parser = subparsers.add_parser('create_db', help='Create database')
+create_db_parser.add_argument('database_name', help='Database name')
+
+drop_db_parser = subparsers.add_parser('drop_db', help='Drop database')
+drop_db_parser.add_argument('database_name', help='Database name')
+
+start_db_parser = subparsers.add_parser('start_db', help='Start database http server')
+start_db_parser.add_argument('database_name', help='Database name')
+start_db_parser.add_argument('-H', help='Host', nargs=1, default='localhost')
+start_db_parser.add_argument('-P', help='Port', nargs=1, default='3471')
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    args.func(args)
